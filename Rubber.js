843e2e1896e890bdddad6918899783c5f3ee7278
chasecrespo <76529894+chasecrespo@users.noreply.github.com>
2021-12-29 09:58:15 -0600

    Update Rubber.js

--- Rubber.js
+++ Rubber.js
@@ -8,7 +8,7 @@ class Rubber{
         this.x=x
         this.y=y
         this.r=r
-        this.body=Bodies.cirlce(this.x,this.y,(this.r-20)/2,options)
+        this.body=Bodies.circle(this.x,this.y,(this.r-20)/2,options)
         World.add(world,this.body)
 
     }
